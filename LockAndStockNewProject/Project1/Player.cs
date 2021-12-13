6f999517fa75748d72cb837c5c355555898a2b8b
underscore830 <70597619+underscore830@users.noreply.github.com>
2021-12-13 14:12:52 -0500

    because of an issue with monogame and visual studios updating I had to copy the content over into a new project

---
+++ LockAndStockNewProject/Project1/Player.cs
@@ -0,0 +1,146 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Audio;
+
+
+
+namespace LockAndStock
+{
+    class Player
+    {
+        private int health;
+        private int speed;
+        private Texture2D texture;
+        private Texture2D projectileTexture;
+        private int score;
+        private List<Bullet> bulletList = new List<Bullet>();
+        private Vector2 shotDirection;
+        private Rectangle position;
+        bool hasShot = false;
+        private bool isInvincible;
+        private SoundEffect shotSound;
+        private Color color = Color.White;
+
+
+
+        public Rectangle Position
+        {
+            get { return position; }
+        }
+
+        public bool IsInvincible
+        {
+            get { return isInvincible; }
+            set { isInvincible = value; }
+        }
+
+        public int Health
+        {
+            get { return health; }
+            set { health = value; }
+        }
+        public List<Bullet> BulletList
+        {
+            get { return bulletList; }
+        }
+        public int Score
+        {
+            get { return score; }
+            set { score = value; }
+        }
+        public bool HasShot
+        {
+            get { return hasShot; }
+            set { hasShot = value; }
+        }
+
+        public int Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+        public Player(int health, int speed, Texture2D texture, Texture2D projectileTexture, Rectangle position, SoundEffect shotSound)
+        {
+            this.health = health;
+            this.speed = speed;
+            this.texture = texture;
+            this.projectileTexture = projectileTexture;
+            this.position = position;
+            this.shotSound = shotSound;
+        }
+
+        public void Update(double gametime, MouseState mouse, KeyboardState kbState)
+        {
+            shotDirection = new Vector2((mouse.X - position.X), (mouse.Y - position.Y));
+            shotDirection.Normalize();
+
+            if (isInvincible == true)
+            {
+                color = Color.Purple;
+            }
+            else
+            {
+                color = Color.White;
+            }
+
+            if (gametime >= 0.5 && mouse.LeftButton == ButtonState.Pressed)
+            {
+
+                bulletList.Add(new Bullet(shotDirection, projectileTexture, new Rectangle(position.X, position.Y, 50, 50)));
+                shotSound.Play(0.1f, 0, 0);
+
+                hasShot = true;
+                Recoil(shotDirection);
+            }
+
+            if (kbState.IsKeyDown(Keys.W))
+            {
+                position.Y -= speed;
+
+            }
+            if (kbState.IsKeyDown(Keys.S))
+            {
+                position.Y += speed;
+
+            }
+            if (kbState.IsKeyDown(Keys.A))
+            {
+                position.X -= speed;
+
+            }
+            if (kbState.IsKeyDown(Keys.D))
+            {
+                position.X += speed;
+
+            }
+        }
+
+        public void Reset()
+        {
+            score = 0;
+            health = 5;
+            position.X = 1920 / 2;
+            position.Y = 1080 / 2;
+            bulletList.Clear();
+            isInvincible = false;
+        }
+
+        private void Recoil(Vector2 Direction)
+        {
+            position.X -= (int)(50 * Direction.X);
+            position.Y -= (int)(50 * Direction.Y);
+        }
+        public virtual void Draw(SpriteBatch sb)
+        {
+            sb.Draw(texture, position, color);
+        }
+    }
+}
---
+++ LockAndStockNewProject/Project1/PowerUp.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Audio;
+
+namespace LockAndStock
+{
+    public enum type
+    {
+        healthUP,
+        invincibility,
+        speedUP,
+        clearScreen
+
+    }
+    class PowerUp
+    {
+        private Rectangle position;
+        private type powerUpType;
+        private Texture2D texture;
+        private bool isActive = true;
+        private bool isVisible = true;
+        private SoundEffect sfx;
+        private bool endInvincible;
+
+        public bool EndInvincible
+        {
+            get { return endInvincible; }
+        }
+
+        public bool IsActive
+        {
+            get { return isActive; }
+        }
+
+        public bool IsVisible
+        {
+            get { return isVisible; }
+        }
+        public PowerUp(Rectangle position, type powerUpType, Texture2D texture, SoundEffect sfx)
+        {
+            this.position = position;
+            this.powerUpType = powerUpType;
+            this.texture = texture;
+            this.sfx = sfx;
+        }
+
+        public void Update(Player player, double timer, List<enemy> enemyList)
+        {
+            if (player.Position.Intersects(position))
+            {
+                if (powerUpType != type.invincibility)
+                {
+                    sfx.Play(1, 0, 0);
+                }
+
+                if (powerUpType == type.healthUP)
+                {
+                    player.Health++;
+                    isActive = false;
+                }
+                else if (powerUpType == type.invincibility)
+                {
+                    endInvincible = false;
+                    if (player.IsInvincible == false)
+                    {
+                        sfx.Play(1, 0, 0);
+                    }
+                    isVisible = false;
+                    player.IsInvincible = true;
+
+
+
+
+                }
+
+                else if (powerUpType == type.speedUP)
+                {
+                    player.Speed++;
+                    isActive = false;
+                }
+
+                else if (powerUpType == type.clearScreen)
+                {
+                    enemyList.Clear();
+                    isActive = false;
+                }
+            }
+
+            if (timer < 0)
+            {
+                player.IsInvincible = false;
+                isActive = false;
+                endInvincible = true;
+
+            }
+        }
+        public virtual void Draw(SpriteBatch sb)
+        {
+            sb.Draw(texture, position, Color.White);
+        }
+
+    }
+}
